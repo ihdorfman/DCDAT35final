7244fc12589ab65108f78be509274e3fe25a389d
Isaac Dorfman <isaacdorfman@Isaacs-MacBook-Pro.local>
2019-10-14 21:34:37 -0400

    started building app

--- app.py
+++ app.py
@@ -20,21 +20,22 @@ logo=base64.b64encode(open('assets/logo.png', 'rb').read())
 
 # Step 2. Create main app layout
 app.layout = html.Div([
-    html.Div([
+    html.Div([html.Div([
         html.H4('Evaluating UFC Fight Data 1993-2019'),
         html.P("Isaac Dorfman - DCDAT35 Capstone Project")],
-        style = {'padding' : '15px' ,'backgroundColor' : '#546D74'}),
+        style = {'padding' : '15px' ,'backgroundColor' : '#546D74'}, className='nine columns'),
+    html.Div([html.Img(src='data:image/png;base64,{}'.format(logo.decode()), style={'width':'270px'}),],className='three columns')],className='twelve columns'),
     html.Br(),
-    dcc.Tabs(id="tabs-template", value='tab-1-template', children=[
+    html.Div([dcc.Tabs(id="tabs-template", value='tab-1-template', children=[
         dcc.Tab(label='Scoring Criteria', value='tab-1-template'),
         dcc.Tab(label='Judge Evaluation', value='tab-2-template'),
         dcc.Tab(label='Simulated Fight', value='tab-3-template'),
-    ]),
+    ])],className='twelve columns'),
     html.Div(id='tabs-content-template')
 ], className='twelve columns')
 
 
-# Step 3a. Create tab callback
+# Step 3. Create tab callback
 
 @app.callback(Output('tabs-content-template', 'children'),
               [Input('tabs-template', 'value')])
@@ -45,8 +46,7 @@ def render_content(tab):
         return tab_2.tab_2_layout
     elif tab == 'tab-3-template':
         return tab_3.tab_3_layout
-    elif tab == 'tab-4-template':
-        return tab_4.tab_4_layout
+
 
 
 
--- tabs/tab_1.py
+++ tabs/tab_1.py
@@ -7,16 +7,16 @@ decision=base64.b64encode(open('assets/decision.png', 'rb').read())
 
 
 tab_1_layout = html.Div([
-    html.Br(),
-    html.Br(),
     html.Div([
     html.Div([
-        dcc.Markdown("Round winner is awarded 10 points with the round loser receiving 9 points or less."),
-        dcc.Markdown("* Scoring is first based on effective striking and/or grappling."),
-        dcc.Markdown("* The second scoring metric is effective aggressiveness in attempting to pursue a finish."),
-        dcc.Markdown("* The final scoring metric is fight area control; e.g. who is dictating where the fight is occuring."),
+        html.Br(),
+            dcc.Markdown("Round winner is awarded 10 points with the round loser receiving 9 points or less."),
+                dcc.Markdown("* Scoring is first based on effective striking and/or grappling."),
+                dcc.Markdown("* The second scoring metric is effective aggressiveness in attempting to pursue a finish."),
+                dcc.Markdown("* The final scoring metric is fight area control; e.g. who is dictating where the fight is occuring."),
         html.A('View code on github', href='https://github.com/ihdorfman/DCDAT35final'),
     ],className='nine columns'),
+    html.Br(),
     html.Div([
     html.Img(src='data:image/png;base64,{}'.format(decision.decode()), style={'height':'300px'}),
     ],className='three columns'),
