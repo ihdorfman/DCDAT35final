df9a8c2b2d566efb96b242b10c54d238a3a85b42
Isaac Dorfman <isaacdorfman@Isaacs-MBP.fios-router.home>
2019-10-19 19:51:16 -0400

    updated formatting

--- tabs/tab_1.py
+++ tabs/tab_1.py
@@ -13,7 +13,7 @@ tab_1_layout = html.Div([
             dcc.Markdown("&nbsp;&nbsp;&nbsp;&nbsp;Round winner is awarded 10 points with the round loser receiving 9 points or less."),
                 dcc.Markdown("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;* Scoring is first based on effective striking and/or grappling."),
                 dcc.Markdown("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;* The second scoring metric is effective aggressiveness in attempting to pursue a finish."),
-                dcc.Markdown("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;* The final scoring metric is fight area control; e.g. who is dictating where the fight is &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;occuring."),
+                dcc.Markdown("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;* The final scoring metric is fight area control; e.g. who is dictating where the fight is occuring."),
         html.A('View code on github', href='https://github.com/ihdorfman/DCDAT35final'),
     ],style={'fontSize':18}, className='nine columns'),
     html.Br(),
